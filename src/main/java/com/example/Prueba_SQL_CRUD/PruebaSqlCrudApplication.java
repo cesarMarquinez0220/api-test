28bcb9b2d0d8584f160f9014726c83d535f5ac97
cesarMarquinez0220 <cesarmarquinez847@gmail.com>
2026-01-05 08:04:17 -0500

    Primer commit: Estructura inicial Spring Boot

---
+++ src/main/java/com/example/Prueba_SQL_CRUD/PruebaSqlCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.Prueba_SQL_CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaSqlCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaSqlCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Prueba_SQL_CRUD/PruebaSqlCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Prueba_SQL_CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaSqlCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
